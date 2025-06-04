1bb2faa630c3503c3e58b5862da44a7ca0ebb9a3
Nathaniel Greenwell <61354864+ntvmb@users.noreply.github.com>
2025-06-04 08:14:18 -0500

    update version number

--- sl_pkg.py
+++ sl_pkg.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.0.8.1"
+__version__ = "0.0.8.2"
 
 import os
 import sys
